7a7f33be3cb8cbd51a9418f40a07bc3c1b7cfa91
Shohruh003 <shohruhazimov0705@gmail.com>
2024-03-20 12:17:44 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import Button from '@mui/material/Button';
 import './App.css';
+import { Stack } from '@mui/material';
+import axios from 'axios';
 
 function App() {
+
+  const OpenClick = () => {
+    axios.post('https://00d0-81-95-239-4.ngrok-free.app/open')
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error); 
+  });
+  }
+
+  const StopClick = () => {
+    axios.post('https://00d0-81-95-239-4.ngrok-free.app/stop')
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error); 
+  });
+  }
+
+  const CloseClick = () => {
+    axios.post('https://00d0-81-95-239-4.ngrok-free.app/close')
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error); 
+  });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stack direction="row" spacing={2}>
+      <Button onClick={OpenClick} sx={{fontSize: '32px', padding: "10px 30px", width: "300px"}} variant="contained" color="success">
+        Open
+      </Button>
+      <Button onClick={CloseClick} sx={{fontSize: '32px', padding: "10px 30px", width: "300px"}} variant="contained" color="info">
+        Close
+      </Button>
+      <Button onClick={StopClick} sx={{fontSize: '32px', padding: "10px 30px", width: "300px"}} variant="contained" color="error">
+        Stop
+      </Button>
+    </Stack>
     </div>
   );
 }
